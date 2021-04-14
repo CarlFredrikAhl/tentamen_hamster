e01fb3ec3ef564963e01621bd7e6ec9412061d9e
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2021-04-14 10:07:43 +0200

    Before dropping hamsters table

--- tentamen_hamster/Hamster.cs
+++ tentamen_hamster/Hamster.cs
@@ -11,14 +11,46 @@ namespace tentamen_hamster
 
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int HamsterId { get; set; }
-        
+
         //Write code in set to start events when activity changes
         public Activity Activity { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        public Gender Gender { get; set; }
+
+        [Column("Gender")]
+        public string _Gender { get; set; }
+
+        [NotMapped]
+        public Gender Gender
+        {
+            get
+            {
+                if (_Gender == "Male")
+                {
+                    return Gender.Male;
+
+                }
+                else
+                {
+                    return Gender.Female;
+                }
+            }
+
+            set
+            {
+                if (_Gender == "Male")
+                {
+                    Gender = Gender.Male;
+
+                }
+                else
+                {
+                    Gender = Gender.Female;
+                }
+            }
+        }
         public string OwnerName { get; set; }
-        public DateTime? TimeCheckedIn { get; set; } 
+        public DateTime? TimeCheckedIn { get; set; }
         public DateTime? TimeLastExercise { get; set; }
     }
 }
--- tentamen_hamster/Migrations/20210413135211_genderset.Designer.cs
+++ tentamen_hamster/Migrations/20210413135211_genderset.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using tentamen_hamster;
 
 namespace tentamen_hamster.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20210413135211_genderset")]
+    partial class genderset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
